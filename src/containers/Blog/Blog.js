094ccdb98f6993f5703f167ae15e18eb531efbe3
TJ Hardin <MrT246007@gmail.com>
2019-04-24 02:29:59 -0500

    Make a "post" const, and return an array of "Post" components.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,22 +6,39 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        post: []
+    }
+
     //We use componentDidMount and make the HTTP request here.
     componentDidMount () {
         /*Axios has a .get method for sending GET request. This method takes at least 1 argument. We use the URL that we get from our dummy page.*/
         axios.get("http://jsonplaceholder.typicode.com/posts")
             .then(response => {
-                console.log(response);
+                this.setState({post: response.data});
+                //console.log(response);
             });
     }
 
     render () {
+        /*------------------
+        Now we make a "post" const, and we want to use this to render all the "Post" components dynamically.
+            -We take thew array of "post" from state, and "map" through them.
+            -We return and array of "Post" component.
+            -We add some properities.
+                -If you look at the JSON data, it has an "id", so we fix the key.
+                -Also it has a "title". 
+                -We pass these to the "Post" component.
+        ------------------*/ 
+        const post = this.state.post.map(post => {
+            return <Post key={post.id} title={post.title} />
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {/*We output the "post" const*/}
+                    {post}
                 </section>
                 <section>
                     <FullPost />
