9fe345a58b5544cf11e6ffa24dbf04e65731156f
TJ Hardin <MrT246007@gmail.com>
2019-04-24 02:06:20 -0500

    Added componentDidMount & Axios .get request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    //We use componentDidMount and make the HTTP request here.
+    componentDidMount () {
+        /*Axios has a .get method for sending GET request. This method takes at least 1 argument. We use the URL that we get from our dummy page.*/
+        axios.get("http://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render () {
         return (
             <div>
