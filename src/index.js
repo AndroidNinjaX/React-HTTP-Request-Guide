15c53c933657f4e9f74f862e2819612ac15497b5
TJ Hardin <MrT246007@gmail.com>
2019-05-27 02:00:59 -0500

    Specify headers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://jsonplaceholder.typicode.com";
+//We could put out authorization token here, if we had one.
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+//We could also specify the content type if we needed to.
+axios.defaults.headers.common["Content-Type"] = 'application.json';
+
 
 axios.interceptors.request.use(request => {
     console.log(request);
