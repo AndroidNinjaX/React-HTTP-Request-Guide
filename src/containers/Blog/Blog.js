7eb626fd41b9b6d1df102bfbb681402ad4095bc4
TJ Hardin <MrT246007@gmail.com>
2019-06-05 01:05:12 -0500

    Change anchor tags to Link components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,8 +12,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            {/*Change "<a herf='/'>" to "Link"*/}
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: "#submit",
+                                search: "?quick-submit=true"
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
